7abe38341488970d2bfc5d543de787b9f6fb2076
juliane1999 <lastjuliane@gmail.com>
2021-09-02 17:50:25 -0700

    made the server, index, and middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,30 @@
 // add middlewares here related to actions
+const Action = require('./actions-model')
+
+async function actionId(req, res, next) {
+
+   try {
+     const action = await Action.get(req.params.id)
+     if (!action) {
+       next({status:404, message: 'action not found'})
+     } else {
+       req.action = action
+       next()
+     }
+   } catch (err) {
+     console.log(err)
+   }
+  }
+
+  function actionName (req, res, next) {
+
+    const {name} = req.body
+    if(!name) {
+      res.status(400).json({
+        message: 'missing required name'}) 
+      }else {
+        next()
+    }
+  }
+
+  module.exports = {actionId,actionName}
--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,30 @@
 // add middlewares here related to projects
+const Project = require('./projects-model')
+
+async function projectId(req, res, next) {
+
+   try {
+     const project = await Project.get(req.params.id)
+     if (!project) {
+       next({status:404, message: 'project not found'})
+     } else {
+       req.project = project
+       next()
+     }
+   } catch (err) {
+     console.log(err)
+   }
+  }
+
+  function projectName (req, res, next) {
+
+    const {name} = req.body
+    if(!name) {
+      res.status(400).json({
+        message: 'missing required name'}) 
+      }else {
+        next()
+    }
+  }
+
+  module.exports = {projectId,projectName}
